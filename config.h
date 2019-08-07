10b2004a0e55d713c170bfee6ccf964c357f5b03
Tomáš Kallup <T.Kallup@gmail.com>
2019-08-07 09:55:19 +0200

    Added newterm patch

--- config.h
+++ config.h
@@ -199,6 +199,7 @@ ResourcePref resources[] = {
 		{ "borderpx",     INTEGER, &borderpx },
 		{ "cwscale",      FLOAT,   &cwscale },
 		{ "chscale",      FLOAT,   &chscale },
+		{ "alpha",        FLOAT,   &alpha },
 };
 
 /*
@@ -229,6 +230,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,      XK_Y,          selpaste,       {.i =  0} },
 	{ ShiftMask,    XK_Insert,     selpaste,       {.i =  0} },
 	{ TERMMOD,      XK_Num_Lock,   numlock,        {.i =  0} },
+	{ TERMMOD,      XK_Return,     newterm,        {.i =  0} },
 	{ ShiftMask,    XK_Page_Up,    kscrollup,      {.i = -1} },
 	{ ShiftMask,    XK_Page_Down,  kscrolldown,    {.i = -1} },
 	{ ControlMask,  XK_J,          kscrollup,      {.i = -1} },
