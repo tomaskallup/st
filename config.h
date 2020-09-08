0723e47b560bf7eea7a423c4a3039a9ee8344408
Tomáš Kallup <T.Kallup@gmail.com>
2020-09-08 11:34:25 +0200

    Added vim patch

--- config.h
+++ config.h
@@ -149,6 +149,19 @@ unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 static unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 258;
+unsigned int const currentBg = 6, buffSize = 2048;
+/// [Vim Browse] Colors for search results currently on screen.
+unsigned int const highlightBg = 160, highlightFg = 15;
+char const wDelS[] = "!\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~", wDelL[] = " \t";
+char *nmKeys [] = {              ///< Shortcusts executed in normal mode
+  "R/Building\nN", "r/Building\n", "X/juli@machine\nN", "x/juli@machine\n",
+  "Q?[Leaving vim, starting execution]\n","F/: error:\nN", "f/: error:\n", "DQf"
+};
+unsigned int const amountNmKeys = sizeof(nmKeys) / sizeof(*nmKeys);
+/// Style of the {command, search} string shown in the right corner (y,v,V,/)
+Glyph styleSearch = {' ', ATTR_ITALIC | ATTR_BOLD_FAINT, 7, 16};
+Glyph style[] = {{' ',ATTR_ITALIC|ATTR_FAINT,15,16}, {' ',ATTR_ITALIC,232,11},
+                 {' ', ATTR_ITALIC, 232, 4}, {' ', ATTR_ITALIC, 232, 12}};
 
 /*
  * Default shape of cursor
@@ -244,6 +257,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,    XK_Insert,     selpaste,         {.i =  0} },
 	{ TERMMOD,      XK_Num_Lock,   numlock,          {.i =  0} },
 	{ TERMMOD,      XK_Return,     newterm,          {.i =  0} },
+  { MODKEY,               XK_c,           normalMode,     {.i =  0} },
 };
 
 /*
